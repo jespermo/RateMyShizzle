e1a70399d49fa5e251ef2961c827957a640d6861
Jesper Møjbæk <jmo@d60.dk>
2013-05-13 18:10:34 +0200

    Added base controller class

--- RateMyShizzle/Controllers/ControllerBase.cs
+++ RateMyShizzle/Controllers/ControllerBase.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DomainModel;
 
 namespace RateMyShizzle.Controllers
 {
     public class ControllerBase : Controller
     {
-
+        public DatabaseDataContext DatabaseContext = new DatabaseDataContext();
     }
 }
